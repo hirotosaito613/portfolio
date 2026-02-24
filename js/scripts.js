44926d7b3c1c53143a2870f5fe21beb2b9ca0b0c
HIROTO SAITO <45475791+hirotosaito613@users.noreply.github.com>
2026-02-25 02:12:55 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', event => {
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
-            offset: 56,
+            offset: 0,
         });
     };
 
